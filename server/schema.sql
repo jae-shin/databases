bda7c933df662d9bfd467f64a98cb7b6ef3baea8
Jae Shin <jae5464@gmail.com>
2016-07-01 16:14:26 -0700

    Fix tables

--- server/schema.sql
+++ server/schema.sql
@@ -8,31 +8,29 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE rooms (
-  id int NOT NULL AUTO_INCREMENT,
-  name varchar(255),
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE rooms (
+--   id int NOT NULL AUTO_INCREMENT,
+--   name varchar(255),
+--   PRIMARY KEY (id)
+-- );
 
 CREATE TABLE messages (
   id int NOT NULL AUTO_INCREMENT,
   user_id int,
-  room_id int,
-  message varchar(255),
+  roomname varchar(255),
+  text varchar(255),
   PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (room_id) REFERENCES rooms(id)
-
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-CREATE TABLE users_rooms (
-  id int NOT NULL AUTO_INCREMENT,
-  user_id int,
-  room_id int,
-  PRIMARY KEY (id),
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (room_id) REFERENCES rooms(id)
-);
+-- CREATE TABLE users_rooms (
+--   id int NOT NULL AUTO_INCREMENT,
+--   user_id int,
+--   room_id int,
+--   PRIMARY KEY (id),
+--   FOREIGN KEY (user_id) REFERENCES users(id),
+--   FOREIGN KEY (room_id) REFERENCES rooms(id)
+-- );
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
