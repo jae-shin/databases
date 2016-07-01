a032bd72b84b301e883cc7656bced63424df86e8
Jae Shin <jae5464@gmail.com>
2016-07-01 15:45:37 -0700

    Rewrite tables in schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,17 +2,6 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  id int NOT NULL AUTO_INCREMENT,
-  user varchar(255),
-  message varchar(255),
-  room varchar(255),
-  PRIMARY KEY (id),
-  FOREIGN KEY (user) REFERENCES users(id),
-  FOREIGN KEY (room) REFERENCES rooms(id)
-
-);
-
 CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(255),
@@ -23,6 +12,17 @@ CREATE TABLE rooms (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(255),
   PRIMARY KEY (id)
+);
+
+CREATE TABLE messages (
+  id int NOT NULL AUTO_INCREMENT,
+  user_id int,
+  room_id int,
+  message varchar(255),
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+
 );
 
 CREATE TABLE users_rooms (
